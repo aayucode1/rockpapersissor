b6dbe7b2a1e237491cdd978d80ec1d7b966b08d8
aayucode1 <159161439+aayucode1@users.noreply.github.com>
2024-03-31 19:06:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+var you;
+var yourscore = 0;
+var opponent ;
+var opponentScore=0;
+
+var choices =["rock","paper","scissors"];
+
+window.onload = function(){
+    for(let i=0; i < 3; i++)
+    {
+       let choice= document.createElement("img");
+       choice.id = choices[i];
+       choice.src=choices[i]+".png";
+       choice.addEventListener("click",selectChoice);
+       document.getElementById("choices").append(choice);
+    }
+}
+ function selectChoice() {
+    you =this.id;
+    document.getElementById("your-choice").src = you+".png";
+    
+    //random for opponent
+    opponent =choices[Math.floor(Math.random()* 3)];
+    document.getElementById("opponent-choice").src = opponent +".png";
+
+    //check for winner 
+    if(you == opponent){
+        yourscore +=1;
+        opponentScore +=1;
+    }
+    else{
+        if (you=="rock") {
+            if(opponent=="scissors"){
+                yourscore +=1;
+            }
+            else if(opponent == "paper"){
+                opponentScore +=1;
+            }
+        }
+        else if (you=="scissors") {
+            if(opponent=="paper"){
+                yourscore +=1;
+            }
+            else if(opponent == "rock"){
+                opponentScore +=1;
+            }
+        }
+        else if (you=="paper") {
+            if(opponent=="rock"){
+                yourscore +=1;
+            }
+            else if(opponent == "scissors"){
+                opponentScore +=1;
+            }
+        }
+    }
+
+    document.getElementById("your-score").innerText = yourscore;
+    document.getElementById("opponent-score").innerText = opponentScore;
+
+ }
